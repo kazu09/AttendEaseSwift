d8b4600ee3bdb6b6e7cb534b9cb7c3cc914021ba
K.Kazu <64839248+kazu09@users.noreply.github.com>
2024-02-10 23:14:38 +0900

    First commit

---
+++ AttendEase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AttendEase
+//
+//  Created by kazu on 2024/02/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
